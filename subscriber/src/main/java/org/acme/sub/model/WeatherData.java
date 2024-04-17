690a6bf33e8f34bfed6be93622dbf67bab112f5b
andreasblendorio <andreasblendorio6@gmail.com>
2024-04-17 16:37:14 +0200

    Added weather data in weather data model

--- subscriber/src/main/java/org/acme/sub/model/WeatherData.java
+++ subscriber/src/main/java/org/acme/sub/model/WeatherData.java
@@ -1,6 +1,9 @@
 package org.acme.sub.model;
 
 import io.quarkus.mongodb.panache.PanacheMongoEntity;
+import io.quarkus.mongodb.panache.common.MongoEntity;
+
+@MongoEntity(collection = "weatherdata")
 
 public class WeatherData extends PanacheMongoEntity {
     public String topic;
